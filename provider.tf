a4bc20ad90f00663589e10bcd3b8278cd6a8b61e
TemitopeAde22 <temitope224468@gmail.com>
2025-09-24 22:23:34 +0100

    Added Terraform Configurations

--- provider.tf
+++ provider.tf
@@ -1,12 +1,18 @@
 terraform {
+  required_version = ">= 1.0"
+  
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "6.14.1"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-2"
 }
